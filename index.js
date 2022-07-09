c204f84545d814138f2a4d89a817f2193c42306f
Bank Panupong <zakam141@gmail.com>
2022-07-10 00:54:14 +0700

    update project ep static file

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
-
-
 const express = require('express')
 const app = express()
-const router = require('./routes/myRouter')
+const path = require('path')
 
-
-app.use(router)
+app.use(express.static(path.join(__dirname,'public')))
 app.listen(process.env.port,process.env.domain,()=>{
     console.log(`start server in port: ${process.env.domain}:${process.env.port}`)
 })
