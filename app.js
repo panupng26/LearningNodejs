020fba008d29447c7414cbc67aa662761573d551
Bank Panupong <zakam141@gmail.com>
2022-07-10 01:25:06 +0700

    update ejs connection template to app.js

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const path = require('path')
+const router = require('./routes/myRouter')
 
 app.set('views',path.join(__dirname, 'views'))
+app.set('view engine','ejs')
+
+app.use(router)
 app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(process.env.port,process.env.domain,()=>{
--- routes/myRouter.js
+++ routes/myRouter.js
@@ -1,7 +1,10 @@
 //manage router
 const express = require('express')
 const router = express.Router()
-const path = require('path')
+
+router.get('/',(req,res)=> {
+    res.render('index.ejs')
+})
 
 
 module.exports = router
