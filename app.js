dcaca3f81ce4d4de8414581dc648a5d6b93e210f
Bank Panupong <zakam141@gmail.com>
2022-07-10 01:20:29 +0700

    template kshop

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+app.set('views',path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname,'public')))
+
 app.listen(process.env.port,process.env.domain,()=>{
     console.log(`start server in port: ${process.env.domain}:${process.env.port}`)
 })
